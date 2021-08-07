149a24de7d4b75f244368d284235a545298e3574
Octacílio <octa.oca44@gmail.com>
2021-08-07 12:03:15 -0300

    Ajustes no css do formulário

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,6 +9,7 @@
    <link rel="preconnect" href="https://fonts.googleapis.com">
    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
    <link href="https://fonts.googleapis.com/css2?family=Arimo:wght@400;500;600;700&family=Exo+2:ital,wght@0,100;0,200;0,300;0,600;0,700;0,800;1,400&display=swap" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css2?family=Rosario:wght@300&display=swap" rel="stylesheet">
 
    <title>Pet Watcher - Login</title>
 </head>
